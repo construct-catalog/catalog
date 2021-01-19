785108cc6e9cf552f96905efc7d2e06e68cfc486
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2021-01-19 11:48:42 +0100

    margin settings (#73)

--- packages/catalog-frontend/src/App.tsx
+++ packages/catalog-frontend/src/App.tsx
@@ -3,17 +3,19 @@ import PackageCard from './PackageCard';
 import * as schema from 'catalog-schema';
 import { Icon, Label, Input, InputOnChangeData } from 'semantic-ui-react'
 import { Grid } from 'semantic-ui-react'
-import { searchByQuery } from './SearchApi';
+import { searchByQuery, getTotalCount } from './SearchApi';
 import logo from './logo.png';
+import './App.css';
 import { Image } from 'semantic-ui-react'
 
-export class App extends React.Component<{}, { packages: schema.Package[], activePage: number }> {
+export class App extends React.Component<{}, { packages: schema.Package[], activePage: number, count: number }> {
 
   constructor(props: any) {
     super(props);
     this.onSearchChange = this.onSearchChange.bind(this);
-    this.state = { packages: [], activePage: 1 };
+    this.state = { packages: [], activePage: 1, count: 0 };
     this.search('');
+    getTotalCount().then(count => this.setState({...this.state, count}))
   }
 
   public render() {
@@ -26,21 +28,21 @@ export class App extends React.Component<{}, { packages: schema.Package[], activ
       cards.push(<PackageCard key={i} package={p}></PackageCard>)
     }
 
-    const cardsPerRow = 'four';
+    const cardsPerRow = 'three';
 
     return (
 
-      <Grid>
+      <Grid padded>
         <Grid.Row className="App-search" centered>
           <Image src={logo} alt="logo" size='small'></Image>
         </Grid.Row>
         <Grid.Row className="App-search" centered>
           <Input onChange={this.onSearchChange}/>
-          <Label>
-            <Icon name='numbered list'/> {packages.length}
+          <Label className="v-middle">
+            <Icon name='numbered list'/> {packages.length}/{this.state.count}
           </Label>
         </Grid.Row>
-        <Grid.Row className={`ui ${cardsPerRow} doubling stackable cards`}>
+        <Grid.Row className={`ui ${cardsPerRow} doubling stackable cards container`}>
           {cards}
         </Grid.Row>
       </Grid>
@@ -56,7 +58,7 @@ export class App extends React.Component<{}, { packages: schema.Package[], activ
   private search(q: string) {
     searchByQuery(q)
       .then(data => {
-        this.setState({ packages: data })
+        this.setState({ ...this.state, packages: data });
       });
   }
 
